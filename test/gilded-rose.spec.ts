99e89a429d0de81915357ac8d4f32b6f4bf5e4ed
Sergiu Pana <sergiu.pana@softwire.com>
2023-06-28 14:29:10 +0300

    Added unit test for Sulfuras

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -9,4 +9,12 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('foo');
     });
 
+
+    it('should update quality of Sulfuras correctly', function () {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 100, 80) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal('Sulfuras, Hand of Ragnaros');
+        expect(items[0].quality).to.equal(80);
+        expect(items[0].sellIn).to.equal(100);
+    });
 });
