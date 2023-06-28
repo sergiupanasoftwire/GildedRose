9b20154cdcf5a1ede069082ff23fde93d30f0d59
Sergiu Pana <sergiu.pana@softwire.com>
2023-06-28 15:22:10 +0300

    Refactored checks for Backstage pass

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -22,6 +22,34 @@ export class GildedRose {
             if (this.items[i].name === 'Sulfuras, Hand of Ragnaros') {
                 continue;
             }
+
+            this.items[i].sellIn -= 1;
+
+            if (this.items[i].name === 'Aged Brie' && this.items[i].quality < 50) {
+                this.items[i].quality += 1;
+                continue;
+            }
+
+            if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
+                if (this.items[i].sellIn < 0) {
+                    this.items[i].quality = 0;
+                    continue;
+                }
+
+                if (this.items[i].sellIn <= 5) {
+                    this.items[i].quality = this.items[i].quality + 3;
+                    continue;
+                }
+
+                if (this.items[i].sellIn <= 10) {
+                    this.items[i].quality = this.items[i].quality + 2;
+                    continue;
+                }
+
+                this.items[i].quality = this.items[i].quality + 1;
+                continue;
+            }
+
             if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     this.items[i].quality = this.items[i].quality - 1
@@ -36,17 +64,10 @@ export class GildedRose {
                                 this.items[i].quality = this.items[i].quality + 1
                             }
                         }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
                     }
                 }
             }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -3,114 +3,56 @@ import {GildedRose, Item} from "../app/gilded-rose";
 
 describe('Golden Master testing', function () {
 
-    it('testing one item update', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
-        expect(items[0].quality).to.equal(0);
-        expect(items[0].sellIn).to.equal(-1);
-    });
+    it('should update all items correctly', () => {
 
-    it('should update quality of item correctly after sell by date', function () {
-        const gildedRose = new GildedRose([ new Item('Car', 0, 10) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('Car');
-        expect(items[0].quality).to.equal(8);
-        expect(items[0].sellIn).to.equal(-1);
-    });
-
-    it('should update quality of Aged Brie correctly', function () {
-        const gildedRose = new GildedRose([ new Item('Aged Brie', 100, 20) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('Aged Brie');
-        expect(items[0].quality).to.equal(21);
-        expect(items[0].sellIn).to.equal(99);
-    });
-
-    it('should not update quality of Aged Brie above 50', function () {
-        const gildedRose = new GildedRose([ new Item('Aged Brie', 100, 50) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('Aged Brie');
-        expect(items[0].quality).to.equal(50);
-        expect(items[0].sellIn).to.equal(99);
-    });
-
-    it('should not drop quality below 0', function () {
-        const gildedRose = new GildedRose([ new Item('Car', 0, 2) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('Car');
-        expect(items[0].quality).to.equal(0);
-        expect(items[0].sellIn).to.equal(-1);
-
-        const updatedItems = gildedRose.updateQuality();
-        expect(updatedItems[0].name).to.equal('Car');
-        expect(updatedItems[0].quality).to.equal(0);
-        expect(updatedItems[0].sellIn).to.equal(-2);
-    });
-
-    it('should update quality of Backstage pass correctly when there are between 5 and 10 days to concert', function () {
-        const gildedRose = new GildedRose(
-            [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 8, 20)
-            ]
-        );
-
-        const updatedItems = gildedRose.updateQuality();
-
-        expect(updatedItems[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
-        expect(updatedItems[0].sellIn).to.equal(7);
-        expect(updatedItems[0].quality).to.equal(22);
-    });
-
-    it('should update quality of Backstage pass correctly when there are between 0 and 5 days to concert', function () {
-        const gildedRose = new GildedRose(
-            [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 3, 20)
-            ]
-        );
-
-        const updatedItems = gildedRose.updateQuality();
-
-        expect(updatedItems[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
-        expect(updatedItems[0].sellIn).to.equal(2);
-        expect(updatedItems[0].quality).to.equal(23);
-    });
-
-    it('should update quality of Backstage pass correctly when there are 0 days to concert', function () {
-        const gildedRose = new GildedRose(
-            [
-                new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20)
-            ]
-        );
-
-        const updatedItems = gildedRose.updateQuality();
-
-        expect(updatedItems[0].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
-        expect(updatedItems[0].sellIn).to.equal(-1);
-        expect(updatedItems[0].quality).to.equal(0);
-    });
-
-    it('should check if random item, Backstage pass and Sulfuras have their values upated correctly', function () {
-        const items: Array<Item> = [
-            new Item('Car', 10, 10),
-            new Item('Backstage passes to a TAFKAL80ETC concert', 20, 20),
-            new Item('Sulfuras, Hand of Ragnaros', 100, 80)
+        const items: Item[] = [
+            new Item('Sulfuras, Hand of Ragnaros', 100, 80),
+            new Item('Aged Brie', 20, 20),
+            new Item('Aged Brie', 20, 50),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 20, 10),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 5, 30),
+            new Item('Backstage passes to a TAFKAL80ETC concert', 0, 40),
+            new Item('Basic item', 0, 0)
         ];
-        const gildedRose: GildedRose = new GildedRose(items);
-        const updatedItems: Array<Item> = gildedRose.updateQuality();
 
-        expect(updatedItems[0].name).to.equal('Car');
-        expect(updatedItems[0].quality).to.equal(9);
-        expect(updatedItems[0].sellIn).to.equal(9);
+        const gildedRose = new GildedRose(items);
 
-        expect(updatedItems[1].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        const updatedItems = gildedRose.updateQuality();
+
+        expect(updatedItems[0].name).to.equal('Sulfuras, Hand of Ragnaros');
+        expect(updatedItems[0].quality).to.equal(80);
+        expect(updatedItems[0].sellIn).to.equal(100);
+
+        expect(updatedItems[1].name).to.equal('Aged Brie');
         expect(updatedItems[1].quality).to.equal(21);
         expect(updatedItems[1].sellIn).to.equal(19);
 
-        expect(updatedItems[2].name).to.equal('Sulfuras, Hand of Ragnaros');
-        expect(updatedItems[2].quality).to.equal(80);
-        expect(updatedItems[2].sellIn).to.eq(100);
-    });
+        expect(updatedItems[2].name).to.equal('Aged Brie');
+        expect(updatedItems[2].quality).to.equal(50);
+        expect(updatedItems[2].sellIn).to.equal(19);
 
+        expect(updatedItems[3].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(updatedItems[3].quality).to.equal(11);
+        expect(updatedItems[3].sellIn).to.equal(19);
+
+        expect(updatedItems[4].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(updatedItems[4].quality).to.equal(22);
+        expect(updatedItems[4].sellIn).to.equal(9);
+
+
+        expect(updatedItems[5].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(updatedItems[5].quality).to.equal(33);
+        expect(updatedItems[5].sellIn).to.equal(4);
+
+        expect(updatedItems[6].name).to.equal('Backstage passes to a TAFKAL80ETC concert');
+        expect(updatedItems[6].quality).to.equal(0);
+        expect(updatedItems[6].sellIn).to.equal(-1);
+
+        expect(updatedItems[7].name).to.equal('Basic item');
+        expect(updatedItems[7].quality).to.equal(0);
+        expect(updatedItems[7].sellIn).to.equal(-1);
+
+    });
 
 });
